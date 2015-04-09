225c16f88d092ed5d601206ac1a13dca7029d760
Infydevops <sandeep_saxena01@infosys.com>
2015-04-08 23:06:47 -0700

    Added a new cookbook from
    http://www.systemcentercentral.com/day-57-a-simple-recipe-and-cookbook-for-windows/

---
+++ cookbooks/W2K12R2_IIS/metadata.rb
@@ -0,0 +1,8 @@
+name             'W2K12R2_IIS'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures W2K12R2_IIS'
+long_description 'Installs/Configures W2K12R2_IIS'
+version          '0.1.0'
+
